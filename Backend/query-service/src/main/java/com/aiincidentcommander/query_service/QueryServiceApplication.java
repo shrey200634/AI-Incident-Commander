e96de730610d61046df8ec2fe9ecb106136e1fa6
shrey200634 <dshrey60@gmail.com>
2026-07-18 01:33:26 +0530

    setup the query side read db

---
+++ Backend/query-service/src/main/java/com/aiincidentcommander/query_service/QueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aiincidentcommander.query_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryServiceApplication.class, args);
+	}
+
+}
---
+++ Backend/query-service/src/test/java/com/aiincidentcommander/query_service/QueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiincidentcommander.query_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
