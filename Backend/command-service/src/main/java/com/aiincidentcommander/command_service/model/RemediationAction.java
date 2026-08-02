7a41a93b8c4a85199b8a0a3912b53f652c6b58d5
shrey200634 <dshrey60@gmail.com>
2026-08-02 19:57:24 +0530

    add the new remedition Action dto into the command service

--- Backend/command-service/src/main/java/com/aiincidentcommander/command_service/model/RemediationAction.java
+++ Backend/command-service/src/main/java/com/aiincidentcommander/command_service/model/RemediationAction.java
@@ -45,6 +45,7 @@ public class RemediationAction {
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+
     @PrePersist
     protected void onCreate() {
         this.createdAt = LocalDateTime.now();
