4ba6d62e7373a0e062b0b5c00018904a45e22b2c
shrey200634 <dshrey60@gmail.com>
2026-08-15 04:10:46 +0530

    add the OpenTelemetryLogbackConfig

--- Backend/command-service/src/main/java/com/aiincidentcommander/command_service/config/OpenTelemetryLogbackConfig.java
+++ Backend/command-service/src/main/java/com/aiincidentcommander/command_service/config/OpenTelemetryLogbackConfig.java
@@ -1,4 +1,21 @@
 package com.aiincidentcommander.command_service.config;
 
-public class OpenTelemetryLogbackConfig {
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OpenTelemetryLogbackConfig implements InitializingBean {
+    private final OpenTelemetry openTelemetry;
+
+    public OpenTelemetryLogbackConfig(OpenTelemetry openTelemetry){
+        this.openTelemetry=openTelemetry;
+    }
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        OpenTelemetryAppender.install(openTelemetry);
+
+    }
 }
