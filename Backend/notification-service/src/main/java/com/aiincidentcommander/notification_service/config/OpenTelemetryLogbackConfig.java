6b21c53d8eb7f1a57324394d9d3d3b60531e39d1
shrey200634 <dshrey60@gmail.com>
2026-08-15 04:43:01 +0530

    added loki logs config in the notification-service

---
+++ Backend/notification-service/src/main/java/com/aiincidentcommander/notification_service/config/OpenTelemetryLogbackConfig.java
@@ -0,0 +1,23 @@
+package com.aiincidentcommander.notification_service.config;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class OpenTelemetryLogbackConfig  implements InitializingBean {
+
+    private OpenTelemetry openTelemetry;
+    public OpenTelemetryLogbackConfig(OpenTelemetry openTelemetry){
+        this.openTelemetry=openTelemetry;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        OpenTelemetryAppender.install(openTelemetry);
+    }
+}
