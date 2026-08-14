2d99b6abc2e2b5a31bbce90d45a529c6e7e7e101
shrey200634 <dshrey60@gmail.com>
2026-08-15 04:59:25 +0530

    add the loki logbook into the api-gateway

---
+++ Backend/api-gateway/src/main/java/com/aiincidentcommander/api_gateway/config/OpenTelemetryLogbackConfig.java
@@ -0,0 +1,23 @@
+package com.aiincidentcommander.api_gateway.config;
+
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OpenTelemetryLogbackConfig implements InitializingBean {
+    private  final OpenTelemetry  openTelemetry;
+
+    public OpenTelemetryLogbackConfig(OpenTelemetry openTelemetry){
+        this.openTelemetry=openTelemetry;
+    }
+
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        OpenTelemetryAppender.install(openTelemetry);
+
+    }
+}
