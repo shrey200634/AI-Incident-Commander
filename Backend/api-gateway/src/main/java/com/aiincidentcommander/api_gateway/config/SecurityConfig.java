358c6f63cb070504c9d71d10339c0f09d0cb9165
shrey200634 <dshrey60@gmail.com>
2026-08-21 20:36:10 +0530

    add the security filter chain in the gateway

--- Backend/api-gateway/src/main/java/com/aiincidentcommander/api_gateway/config/SecurityConfig.java
+++ Backend/api-gateway/src/main/java/com/aiincidentcommander/api_gateway/config/SecurityConfig.java
@@ -3,9 +3,17 @@ package com.aiincidentcommander.api_gateway.config;
 
 import com.aiincidentcommander.api_gateway.security.AppUserDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -14,6 +22,32 @@ public class SecurityConfig {
 
     private final AppUserDetailsService appUserDetailsService ;
     private final PasswordEncoder passwordEncoder ;
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(appUserDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config ) throws  Exception{
+        return  config.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http ) throws Exception{
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm ->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .authenticationProvider(authenticationProvider());
+        return http.build();
+    }
 
 }
 
