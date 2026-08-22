3e0c423443b0ec0ad2a2fbcf3365479562551d5c
shrey200634 <dshrey60@gmail.com>
2026-08-22 19:07:19 +0530

    add the swagger open api spring doc in the command side , query side , agent side and the gateway

---
+++ Backend/agent-service/src/main/java/com/aiincidentcommander/agent_service/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.aiincidentcommander.agent_service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI().info(new Info()
+                .title("agent Service")
+                .description("agent -side || API: matrices and the ai ")
+                .version("v1"));
+    }
+}
